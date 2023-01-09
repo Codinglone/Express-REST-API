c0584bbdce817ab85e6ed0328a8efc1fc659815a
Fabrice <codinglone@gmail.com>
2023-01-09 19:59:59 +0200

    Initial commit

---
+++ queries.js
@@ -0,0 +1,80 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: 'root',
+    host: 'localhost',
+    database: 'codinglone',
+    password: 'fab3227423',
+    port: 5432,
+})
+
+const bcrypt = require('bcrypt');
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY email ASC', (error, results) => {
+        if(error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const createUser = (request, response) => {
+    const { password, email } = request.body;
+    const hashedPassword = bcrypt.hash(password, 10);
+
+    pool.query('INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *', [email, hashedPassword], (error, results) => {
+        if(error){
+            throw error;
+        }
+
+        response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+    })
+}
+
+const updateUser = (request, response) => {
+    const id = parseInt(request.params.id);
+    const { email, password } = request.body;
+
+    pool.query('UPDATE users SET email=$1, password=$2 WHERE id = $3', [email, password, id], (error, results) => {
+        if(error){
+            throw error;
+        }
+
+        response.status(200).send(`User modified with ID: ${id}`)
+    })
+}
+
+const deleteUser = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+        if(error){
+            throw error;
+        }
+
+        response.status(200).send(`User deleted with ID: ${id}`)
+    })
+}
+
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+}
+
